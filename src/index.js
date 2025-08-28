e76f079ba95f658e619057faa8982e7892270131
TongLongDev <tonglong.dev@gmail.com>
2025-08-28 15:02:40 +0700

    test gpt-5 nano

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import { OpenAI } from "openai";
+
+const client = new OpenAI();
+// const client = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+
+const prompt = "How big is the moon? Limit your response in 100 words or less.";
+const sysOpt = "";
+
+const response = await client.responses.create({
+  model: "gpt-5-nano",
+  input: [
+    // {
+    //   role: "system",
+    //   content: sysOpt,
+    // },
+    {
+      role: "user",
+      content: prompt,
+    },
+  ],
+  //   max_tokens: 60,
+  //   n: 2,
+  //   frequency_penalty: 1.5,
+  //   seed: 88888,
+});
+
+console.log(response);
+// console.log(response.output[0].content[0].text)
