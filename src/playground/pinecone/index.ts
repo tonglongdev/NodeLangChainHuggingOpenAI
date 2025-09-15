f4a4dbefd8d829750775c426ea387b0dc58a8766
TongLongDev <tonglong.dev@gmail.com>
2025-09-15 19:03:11 +0700

    pinecone integrate

---
+++ src/playground/pinecone/index.ts
@@ -0,0 +1,87 @@
+import { Pinecone } from "@pinecone-database/pinecone";
+
+if (!process.env.PINECONE_API_KEY) {
+  throw new Error("PINECONE_API_KEY is not set");
+}
+
+const pinecone = new Pinecone({
+  apiKey: process.env.PINECONE_API_KEY,
+});
+
+type MetaData = {
+  name: string;
+  age: number;
+};
+
+// namespace: partition vector data from an index into multiple namespaces or groups. This will make the operations limited to a specific namespace.
+const createNameSpace = async () => {
+  const index = getIndex("chat-app-index");
+  const namespace = index.namespace("chat-app-namespace");
+};
+
+const listIndexes = async () => {
+  const indexes = await pinecone.listIndexes();
+  console.log(indexes);
+};
+
+const getIndex = (indexName: string) => {
+  const index = pinecone.index<MetaData>(indexName);
+  console.log(index);
+  return index;
+};
+
+// Generate random vectors
+const generateRandomVectors = (dimension: number) => {
+  return Array.from({ length: dimension }, () => Math.random());
+};
+
+const upsertVectors = async () => {
+  const embedding = generateRandomVectors(1536);
+  const index = getIndex("chat-app-index");
+
+  await index.upsert([
+    {
+      id: "id-2",
+      values: embedding,
+      metadata: {
+        name: "John Doe",
+        age: 25,
+      },
+    },
+  ]);
+  console.log("upsert successful");
+};
+
+const createIndex = async () => {
+  await pinecone.createIndex({
+    name: "chat-app-index",
+    dimension: 1536,
+    metric: "cosine",
+    spec: {
+      serverless: {
+        cloud: "aws",
+        region: "us-east-1",
+      },
+    },
+  });
+};
+
+// const queryVectors = async () => {
+//   const index = getIndex("bgwebagency-chat-app-index");
+//   const results = await index.query({
+//     id: "id-2",
+//     // how many query results to return
+//     topK: 1,
+//     includeMetadata: true,
+//   });
+//   console.log(results);
+// };
+
+const main = async () => {
+  // await listIndexes();
+  // getIndex("chat-app-index");
+  await upsertVectors();
+  // await queryVectors();
+};
+
+main();
